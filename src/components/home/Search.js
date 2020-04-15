f917348acaede49d56535abc7b70a76372d0b677
ca2roaldsoy <roaldsoyen@gmail.com>
2020-04-15 10:34:49 +0200

    cleaned up css

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 // Searchbar
 function Search({ handleInput }) {
@@ -12,6 +13,7 @@ function Search({ handleInput }) {
         onChange={(e) => handleInput(e)}
         role="search-input"
       />
+      <Button variant="outline-primary">Search</Button>
     </InputGroup>
   );
 }
